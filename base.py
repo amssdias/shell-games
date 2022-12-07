7235846fb791ade7c4efb19663805990eb3eaf85
André Dias <andremssdias@hotmail.com>
2022-12-07 16:22:44 +0100

    feat(base): Let user choose a game

--- base.py
+++ base.py
@@ -1,15 +1,22 @@
 from games import Hangman, BattleShip
 
 games = {
-    "hangman": Hangman,
-    "battleship": BattleShip,
+    "Hangman": Hangman,
+    "Battleship": BattleShip,
 }
-
-game = Hangman()
 
 while True:
 
-    game.play()
+    print("Hello! Which game you would like to play?")
+
+    for game in games.keys():
+        print(f"1 - {game}")
+
+    user_game = input("Write the name of the game: ")
+
+    game = games[user_game.capitalize()]
+
+    game().play()
 
     user_answer = input("Would you like to retry? (Y/N)")
     if user_answer in ["Y", "y"]:
