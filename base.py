474a2bc8bfb93e9cd50be26ebf695b72e1abf31c
André Dias <andremssdias@hotmail.com>
2023-01-02 22:54:52 +0100

    change name of base file to main

--- base.py
+++
@@ -1,9 +0,0 @@
-from main import settings
-from main.controller import Controller
-
-from models.player import Player
-
-
-player_model = Player(settings.DATABASE)
-c = Controller(player_model)
-c.start()
