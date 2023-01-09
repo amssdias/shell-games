44121b823bd012ec872a3a754823041960389275
André Dias <andremssdias@hotmail.com>
2023-01-09 12:59:10 +0100

    Refactor Menu tests
    This commit refactor the tests for the Menu module.
    - Use 'builtins' to mock prints and inputs.
    - Mock methods that were being used in another methods.
    - Add tests to assert the mock objects were being called

--- tests/controller/test_menu.py
+++ tests/controller/test_menu.py
@@ -1,9 +1,10 @@
 from colorama import Fore
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
-from main.menu import menu_options
-from main.menu import Menu
+from main.login import Login
+from main.menu import Menu, menu_options
+from main.signup import Signup
 
 
 
@@ -11,7 +12,7 @@ class TestMenu(unittest.TestCase):
 
     @patch("main.menu.Player")
     @patch("main.menu.Menu.initial_menu")
-    @patch("main.menu.print")
+    @patch("builtins.print")
     def setUp(self, mocked_print, mocked_initial_menu, mocked_player) -> None:
         self.menu = Menu()
 
@@ -19,15 +20,23 @@ class TestMenu(unittest.TestCase):
         menu_initialize_variables = self.menu.__dict__.keys()
         self.assertIn("player", menu_initialize_variables)
 
-    def _test_initial_menu(self):
-        pass
+    def test_initial_menu(self):
+        menu_option = MagicMock()
+        self.menu.display_initial_menu = MagicMock(return_value=None)
+        self.menu.get_user_option = MagicMock(return_value=menu_option)
+
+        self.menu.initial_menu(self.menu.player)
+
+        self.menu.display_initial_menu.assert_called_once()
+        self.menu.get_user_option.assert_called_once()
+        menu_option.run.assert_called_once()
     
-    @patch("main.menu.print")
+    @patch("builtins.print")
     def test_display_welcome_message(self, mocked_print):
         self.menu.display_welcome_message()
         self.assertEqual(mocked_print.mock_calls[0].args[0], Fore.GREEN + "Hi, welcome to shell games!")
 
-    @patch("main.menu.print")
+    @patch("builtins.print")
     def test_display_initial_menu(self, mocked_print):
         self.menu.display_initial_menu()
         values_printed = [Fore.GREEN + "What would you like to do?"]
@@ -37,29 +46,63 @@ class TestMenu(unittest.TestCase):
         for index, called in enumerate(mocked_print.mock_calls):
             self.assertEqual(called.args[0], values_printed[index])
 
-    @patch("main.menu.input")
+        self.assertEqual(mocked_print.call_count, 3)
+
+    @patch("builtins.input")
     def test_get_user_option_signup(self, mocked_input):
-        mocked_input.return_value = "signup"
-        self.assertTrue(self.menu.get_user_option())
+        self.menu.validate_user_option = MagicMock(return_value="Signup")
+        result = self.menu.get_user_option()
 
-    @patch("main.menu.print")
-    @patch("main.menu.input")
-    def test_get_user_option_signup_wrong(self, mocked_input, mocked_print):
-        mocked_input.return_value = "sign up"
-        self.assertFalse(self.menu.get_user_option())
+        self.assertTrue(result)
+        self.assertIsInstance(result, Signup)
+        mocked_input.assert_called_once()
+        self.menu.validate_user_option.assert_called_once()
 
-    @patch("main.menu.input")
+    @patch("builtins.input")
+    def test_get_user_option_signup_wrong(self, mocked_input):
+        self.menu.validate_user_option = MagicMock(return_value=False)
+        result = self.menu.get_user_option()
+
+        self.assertFalse(result)
+        self.menu.validate_user_option.assert_called_once()
+        mocked_input.assert_called_once()
+
+    @patch("builtins.input")
     def test_get_user_option_login(self, mocked_input):
-        mocked_input.return_value = "login"
-        self.assertTrue(self.menu.get_user_option())
+        self.menu.validate_user_option = MagicMock(return_value="Login")
+        result = self.menu.get_user_option()
 
-    @patch("main.menu.print")
-    @patch("main.menu.input")
-    def test_get_user_option_login_wrong(self, mocked_input, mocked_print):
-        mocked_input.return_value = "log in"
-        self.assertFalse(self.menu.get_user_option())
+        self.assertTrue(result)
+        self.assertIsInstance(result, Login)
+        self.menu.validate_user_option.assert_called_once()
+        mocked_input.assert_called_once()
 
-    @patch("main.menu.print")
-    def test_validate_user_option(self, mocked_print):
+    @patch("builtins.input", return_value="log in")
+    def test_get_user_option_login_wrong(self, mocked_input):
+        self.menu.validate_user_option = MagicMock(return_value=False)
+        result = self.menu.get_user_option()
+
+        self.assertFalse(result)
+        self.menu.validate_user_option.assert_called_once()
+        mocked_input.assert_called_once()
+
+    @patch("builtins.print")
+    def test_validate_user_option_signup(self, mocked_print):
         validated = self.menu.validate_user_option("signup")
+
         self.assertEqual(validated, "Signup")
+        mocked_print.assert_not_called()
+
+    @patch("builtins.print")
+    def test_validate_user_option_login(self, mocked_print):
+        validated = self.menu.validate_user_option("login")
+        self.assertEqual(validated, "Login")
+        mocked_print.assert_not_called()
+
+    @patch("builtins.print")
+    def test_validate_user_option_wrong_option(self, mocked_print):
+        validated = self.menu.validate_user_option("abcd")
+
+        self.assertFalse(validated)
+        mocked_print.assert_called_once()
+        mocked_print.assert_called_once_with(Fore.RED + "Option not available.")
