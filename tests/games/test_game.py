4762a3ba586c328fcbf734a0d3e7a1cea9b1709e
André Dias <andremssdias@hotmail.com>
2023-01-09 23:10:30 +0100

    Test Game abstract class

--- tests/games/test_game.py
+++ tests/games/test_game.py
@@ -4,19 +4,22 @@ from unittest.mock import patch
 from games.game import Game
 
 
-class TestGameABC(unittest.TestCase):
+class TestGame(unittest.TestCase):
     """
     If you set __abstractmethods__ attribute to be an empty set you'll be able to instantiate abstract class.
     """
 
+    def setUp(self) -> None:
+        Game.__abstractmethods__ = set()
+        self.game = Game()
+        return super().setUp()
+
     @patch("games.game.Game.start_game_settings", return_value=None)
     @patch("games.game.Game.start_game", return_value=True)
     def test_play_won(self, mocked_start_game, mocked_game_settings):
-        Game.__abstractmethods__ = set()
-        self.assertTrue(Game().play())
+        self.assertTrue(self.game.play())
 
     @patch("games.game.Game.start_game_settings", return_value=None)
     @patch("games.game.Game.start_game", return_value=False)
     def test_play_lost(self, mocked_start_game, mocked_game_settings):
-        Game.__abstractmethods__ = set()
-        self.assertFalse(Game().play())
+        self.assertFalse(self.game.play())
