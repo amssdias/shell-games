2af282567f0f14079feb08e3b15e11af3ce3c583
André Dias <andremssdias@hotmail.com>
2023-02-21 21:36:40 +0100

    Delete validate file method and reuse from parent class.

--- models/excel_model.py
+++ models/excel_model.py
@@ -5,13 +5,8 @@ from models.files_models.excel_file import ExcelFile
 
 
 class ExcelDB(FileDB):
+    ext = ".xlsx"
+
     def __init__(self, file_path: Path = Path(Path.cwd(), "data.xlsx")):
         self.validate_file(file_path)
         super().__init__(file_type=ExcelFile(file_path=file_path))
-
-    def validate_file(self, file_path: Path):
-        if file_path.suffix != ".xlsx":
-            raise Exception("File should be a .xlsx extension.")
-        if not file_path.exists():
-            raise Exception("File path does not exist.")
-        return True
